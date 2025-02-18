be8c3918f7f6f98888a82dd48dd3f2e0fd8d730b
J.W.L. Matthé <69607411+jaspermatthe@users.noreply.github.com>
2025-02-18 13:27:23 -0800

    updated max x and y grid points

--- Configuration.h
+++ Configuration.h
@@ -1843,8 +1843,8 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
-  #define GRID_MAX_POINTS_Y 6
+  #define GRID_MAX_POINTS_X 12
+  #define GRID_MAX_POINTS_Y 15
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
